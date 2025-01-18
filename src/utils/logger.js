a071e02f42698115102a2d0797ee0071d83d382c
VictoryTrump <mrzerozero45@gmail.com>
2025-01-18 20:29:45 +0700

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,11 @@
+// src/utils/logger.js
+
+const logInfo = (message) => {
+  console.log(`[INFO]: ${message}`);
+};
+
+const logError = (message) => {
+  console.error(`[ERROR]: ${message}`);
+};
+
+module.exports = { logInfo, logError };
