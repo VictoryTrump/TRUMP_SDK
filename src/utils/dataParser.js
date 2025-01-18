d84bb4aba3222b3369e5b008dd814f33b5140b91
VictoryTrump <mrzerozero45@gmail.com>
2025-01-18 20:30:50 +0700

    Create dataParser.js

---
+++ src/utils/dataParser.js
@@ -0,0 +1,9 @@
+// src/utils/dataParser.js
+
+const parseData = (data) => {
+  // Logic to parse data
+  console.log(`Parsing data: ${JSON.stringify(data)}`);
+  return data; // Returning parsed data as an example
+};
+
+module.exports = { parseData };
